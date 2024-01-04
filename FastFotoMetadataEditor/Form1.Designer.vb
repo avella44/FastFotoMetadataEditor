fcc846bf8db9b46cd10372607c624eff3c0936cc
Anthony <Anthony@DESKTOP-0E0U7E6>
2024-01-04 11:35:08 -0600

    Add project files.

---
+++ FastFotoMetadataEditor/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        BrowseButton = New Button()
+        FileListView = New ListView()
+        Filename1 = New ColumnHeader()
+        DateTaken1 = New ColumnHeader()
+        Modified1 = New ColumnHeader()
+        Button1 = New Button()
+        Button2 = New Button()
+        FileSystemWatcher1 = New IO.FileSystemWatcher()
+        TextBox1 = New TextBox()
+        LoadFolderButton = New Button()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        DateTimePicker1 = New DateTimePicker()
+        RotateLeftButton1 = New Button()
+        RotateLeftButton2 = New Button()
+        RotateRightButton1 = New Button()
+        RotateRightButton2 = New Button()
+        CType(FileSystemWatcher1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' BrowseButton
+        ' 
+        BrowseButton.Location = New Point(1042, 27)
+        BrowseButton.Name = "BrowseButton"
+        BrowseButton.Size = New Size(75, 23)
+        BrowseButton.TabIndex = 1
+        BrowseButton.Text = "Browse"
+        BrowseButton.UseVisualStyleBackColor = True
+        ' 
+        ' FileListView
+        ' 
+        FileListView.Columns.AddRange(New ColumnHeader() {Filename1, DateTaken1, Modified1})
+        FileListView.Location = New Point(39, 88)
+        FileListView.Name = "FileListView"
+        FileListView.Size = New Size(380, 571)
+        FileListView.TabIndex = 3
+        FileListView.UseCompatibleStateImageBehavior = False
+        FileListView.View = View.Details
+        ' 
+        ' Filename1
+        ' 
+        Filename1.Text = "Filename"
+        Filename1.Width = 200
+        ' 
+        ' DateTaken1
+        ' 
+        DateTaken1.Text = "Date taken"
+        DateTaken1.Width = 100
+        ' 
+        ' Modified1
+        ' 
+        Modified1.Text = "Edited"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(1149, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(1230, 12)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' FileSystemWatcher1
+        ' 
+        FileSystemWatcher1.EnableRaisingEvents = True
+        FileSystemWatcher1.SynchronizingObject = Me
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(39, 27)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(997, 23)
+        TextBox1.TabIndex = 7
+        ' 
+        ' LoadFolderButton
+        ' 
+        LoadFolderButton.Location = New Point(39, 56)
+        LoadFolderButton.Name = "LoadFolderButton"
+        LoadFolderButton.Size = New Size(134, 23)
+        LoadFolderButton.TabIndex = 8
+        LoadFolderButton.Text = "Load Folder Contents"
+        LoadFolderButton.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BorderStyle = BorderStyle.FixedSingle
+        PictureBox1.Location = New Point(515, 88)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(580, 580)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 9
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BorderStyle = BorderStyle.FixedSingle
+        PictureBox2.Location = New Point(1124, 88)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(580, 580)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 10
+        PictureBox2.TabStop = False
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.CustomFormat = ""
+        DateTimePicker1.Format = DateTimePickerFormat.Short
+        DateTimePicker1.Location = New Point(194, 56)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(112, 23)
+        DateTimePicker1.TabIndex = 11
+        ' 
+        ' RotateLeftButton1
+        ' 
+        RotateLeftButton1.Location = New Point(715, 59)
+        RotateLeftButton1.Name = "RotateLeftButton1"
+        RotateLeftButton1.Size = New Size(80, 23)
+        RotateLeftButton1.TabIndex = 12
+        RotateLeftButton1.Text = "Rotate Left"
+        RotateLeftButton1.TextImageRelation = TextImageRelation.ImageAboveText
+        RotateLeftButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RotateLeftButton2
+        ' 
+        RotateLeftButton2.Location = New Point(1335, 59)
+        RotateLeftButton2.Name = "RotateLeftButton2"
+        RotateLeftButton2.Size = New Size(80, 23)
+        RotateLeftButton2.TabIndex = 12
+        RotateLeftButton2.Text = "Rotate Left"
+        RotateLeftButton2.TextImageRelation = TextImageRelation.ImageAboveText
+        RotateLeftButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RotateRightButton1
+        ' 
+        RotateRightButton1.Location = New Point(801, 59)
+        RotateRightButton1.Name = "RotateRightButton1"
+        RotateRightButton1.Size = New Size(80, 23)
+        RotateRightButton1.TabIndex = 12
+        RotateRightButton1.Text = "Rotate Right"
+        RotateRightButton1.TextImageRelation = TextImageRelation.ImageAboveText
+        RotateRightButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RotateRightButton2
+        ' 
+        RotateRightButton2.Location = New Point(1421, 59)
+        RotateRightButton2.Name = "RotateRightButton2"
+        RotateRightButton2.Size = New Size(80, 23)
+        RotateRightButton2.TabIndex = 12
+        RotateRightButton2.Text = "Rotate Right"
+        RotateRightButton2.TextImageRelation = TextImageRelation.ImageAboveText
+        RotateRightButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1784, 861)
+        Controls.Add(RotateLeftButton2)
+        Controls.Add(RotateRightButton2)
+        Controls.Add(RotateRightButton1)
+        Controls.Add(RotateLeftButton1)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(LoadFolderButton)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(FileListView)
+        Controls.Add(BrowseButton)
+        Name = "Form1"
+        Text = "Form1"
+        CType(FileSystemWatcher1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BrowseButton As Button
+    Friend WithEvents FileListView As ListView
+    Friend WithEvents Filename1 As ColumnHeader
+    Friend WithEvents DateTaken1 As ColumnHeader
+    Friend WithEvents Modified1 As ColumnHeader
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LoadFolderButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents RotateLeftButton1 As Button
+    Friend WithEvents RotateLeftButton2 As Button
+    Friend WithEvents RotateRightButton2 As Button
+    Friend WithEvents RotateRightButton1 As Button
+
+End Class
---
+++ FastFotoMetadataEditor/Form1.vb
@@ -0,0 +1,335 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Configuration
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Drawing.Imaging
+
+Public Class Form1
+    Dim loaded_image_filepath As String = ""
+    Dim loaded_secondary_image_filepath As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "C:\Users\Anthony\Desktop\FastFoto pics for testing\1989_for_testing"
+
+    End Sub
+
+
+    Private Sub GetFilesInDirectory()
+        FileListView.Items.Clear()
+        FileListView.Items.Add("Loading folder contents...")
+
+        Dim strFileSize As String = ""
+        Dim di As New IO.DirectoryInfo(TextBox1.Text)
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.jpg")
+        Dim fi As IO.FileInfo
+
+        Dim directoryDateTakenStrings As String = ExifGetImageDateTaken(TextBox1.Text)
+
+        Dim regexString As String
+        Dim regex As Regex
+
+        FileListView.Items.Clear()
+        For Each fi In aryFi
+            If fi.Name.Substring(fi.Name.Length - 6, 2) <> "_b" Then
+                Debug.WriteLine(fi.Name)
+
+                Dim item = New ListViewItem(fi.Name)
+
+                'Parse results
+                regexString = "(?<=" & fi.Name & "\r\nDateTimeOriginal: )[0-9]{2}/[0-9]{2}/[0-9]{4}"
+                regex = New Regex(regexString)
+                Dim match As Match = regex.Match(directoryDateTakenStrings)
+                If match.Success Then
+                    item.SubItems.Add(match.Value)
+                Else
+                    item.SubItems.Add("")
+                End If
+
+                item.SubItems.Add("")
+
+                FileListView.Items.Add(item)
+            End If
+
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetFilesInDirectory()
+        'ExifGetImageDateTaken(DirectoryTextBox.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Debug.WriteLine(FileListView.Items(0).SubItems(2).Text)
+        'FileListView.Items(0).SubItems(2).Text = "Yes"
+        RotateImageRight(loaded_image_filepath)
+        DisplayImages()
+    End Sub
+
+
+    Private Shared Function ExifGetImageDateTaken(directory As String) As String
+        If directory(directory.Length - 1) <> "\" Then
+            directory &= "\"
+        End If
+
+        Dim processOutput As New StringBuilder()
+
+        Dim ExifToolGetDateTaken As New Process
+
+        ExifToolGetDateTaken.StartInfo.FileName = "C:\WINDOWS\exiftool.exe"
+        ExifToolGetDateTaken.StartInfo.Arguments = "-if ""$DateTimeOriginal"" -DateTimeOriginal -fast -S -ext jpg -d ""%m/%d/%Y"" """ + directory + "."""
+        ExifToolGetDateTaken.StartInfo.UseShellExecute = False
+        ExifToolGetDateTaken.StartInfo.RedirectStandardOutput = True
+        ExifToolGetDateTaken.StartInfo.CreateNoWindow = True
+
+        ExifToolGetDateTaken.Start()
+        Dim result As String = ExifToolGetDateTaken.StandardOutput.ReadToEnd()
+        'pbx.Image = New Bitmap(ExifTool.StandardOutput.BaseStream).GetThumbnailImage(120, 80, Nothing, Nothing)
+        ExifToolGetDateTaken.Close()
+
+        Debug.WriteLine(result)
+        Return result
+
+    End Function
+
+    Private Sub BrowseButton_Click(sender As Object, e As EventArgs) Handles BrowseButton.Click
+        FolderBrowserDialog1.InitialDirectory = TextBox1.Text
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub DirectoryTextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        'If Not TextBox1.Focused Then
+        '    GetFilesInDirectory()
+        'End If
+        Debug.WriteLine("event")
+
+    End Sub
+
+    Private Sub LoadFolderButton_Click(sender As Object, e As EventArgs) Handles LoadFolderButton.Click
+        GetFilesInDirectory()
+    End Sub
+
+    Private Sub FileListView_SelectedIndexChanged(sender As Object, e As EventArgs) Handles FileListView.SelectedIndexChanged
+        'Display photo when selected
+        If FileListView.SelectedItems.Count > 0 Then
+            DisplayImages()
+        End If
+    End Sub
+
+    Public Shared Function GetMemoryBitmapFromFile(path As String) As Bitmap
+        Dim bm As Bitmap
+        Using img As Image = Image.FromFile(path)
+            'Rotate image if EXIF orientation data exists (https://stackoverflow.com/questions/19306564/correcting-image-orientation-server-side-in-vb-net)
+            Dim rft As RotateFlipType = RotateFlipType.RotateNoneFlipNone
+            For Each p As PropertyItem In img.PropertyItems
+                If p.Id = 274 Then
+                    Dim orientation As Short = BitConverter.ToInt16(p.Value, 0)
+                    Select Case orientation
+                        Case 1
+                            rft = RotateFlipType.RotateNoneFlipNone
+                        Case 3
+                            rft = RotateFlipType.Rotate180FlipNone
+                        Case 6
+                            rft = RotateFlipType.Rotate90FlipNone
+                        Case 8
+                            rft = RotateFlipType.Rotate270FlipNone
+                    End Select
+                End If
+            Next
+            If rft <> RotateFlipType.RotateNoneFlipNone Then
+                img.RotateFlip(rft)
+            End If
+
+            'Create a copy of the image. The original image is released when the "Using" block is exited, so that the file will no longer be locked for editing.
+            bm = New Bitmap(img)
+        End Using
+        Return bm
+    End Function
+
+    Private Sub DisplayImages()
+        'Display main photo
+        Dim filename = FileListView.SelectedItems(0).Text
+        Dim directory = TextBox1.Text
+        If directory(directory.Length - 1) <> "\" Then
+            directory &= "\"
+        End If
+
+        Dim fullFilepath As String = directory & filename
+        PictureBox1.Image = GetMemoryBitmapFromFile(fullFilepath)
+        loaded_image_filepath = fullFilepath
+
+        'Display "_b" photo if it exists
+        fullFilepath = fullFilepath.Replace(".jpg", "_b.jpg")
+        Dim secondary_image_exists As Boolean = File.Exists(fullFilepath)
+        If secondary_image_exists Then
+            loaded_secondary_image_filepath = fullFilepath
+            PictureBox2.Image = GetMemoryBitmapFromFile(fullFilepath)
+        Else
+            loaded_secondary_image_filepath = ""
+            PictureBox2.Image = Nothing
+        End If
+    End Sub
+
+    Private Function GetImageOrientation(fullFilepath As String) As Integer
+        ' Returns the EXIF data for the orientation of a JPG image
+        ' 0 = no metadata exists
+        ' 1 = Horizontal (normal)
+        ' 2 = Mirror horizontal
+        ' 3 = Rotate 180
+        ' 4 = Mirror vertical
+        ' 5 = Mirror horizontal And rotate 270 CW
+        ' 6 = Rotate 90 CW
+        ' 7 = Mirror horizontal And rotate 90 CW
+        ' 8 = Rotate 270 CW
+
+        Dim processOutput As New StringBuilder()
+
+        Dim ExifToolGetDateTaken As New Process
+
+        ExifToolGetDateTaken.StartInfo.FileName = "C:\WINDOWS\exiftool.exe"
+        ExifToolGetDateTaken.StartInfo.Arguments = "-orientation# -S """ + fullFilepath + """"
+        ExifToolGetDateTaken.StartInfo.UseShellExecute = False
+        ExifToolGetDateTaken.StartInfo.RedirectStandardOutput = True
+        ExifToolGetDateTaken.StartInfo.CreateNoWindow = True
+
+        ExifToolGetDateTaken.Start()
+        Dim result As String = ExifToolGetDateTaken.StandardOutput.ReadToEnd()
+        ExifToolGetDateTaken.Close()
+
+        'Parse result
+        Dim regex As Regex
+        regex = New Regex("(?<=Orientation: )[1-8]")
+        Dim match As Match = regex.Match(result)
+        If match.Success Then
+            Return Convert.ToInt32(match.Value)
+        Else
+            Return 0
+        End If
+
+    End Function
+
+    Private Function SetImageOrientation(fullFilepath As String, new_orientation As Integer) As Boolean
+        ' Set the EXIF data for the orientation of a JPG image
+        ' 1 = Horizontal (normal)
+        ' 2 = Mirror horizontal
+        ' 3 = Rotate 180
+        ' 4 = Mirror vertical
+        ' 5 = Mirror horizontal And rotate 270 CW
+        ' 6 = Rotate 90 CW
+        ' 7 = Mirror horizontal And rotate 90 CW
+        ' 8 = Rotate 270 CW
+
+        Dim processOutput As New StringBuilder()
+
+        Dim ExifToolGetDateTaken As New Process
+
+        ExifToolGetDateTaken.StartInfo.FileName = "C:\WINDOWS\exiftool.exe"
+        ExifToolGetDateTaken.StartInfo.Arguments = "-orientation#=" & new_orientation.ToString("0") & " -S """ + fullFilepath + """ -overwrite_original"
+        ExifToolGetDateTaken.StartInfo.UseShellExecute = False
+        ExifToolGetDateTaken.StartInfo.RedirectStandardOutput = True
+        ExifToolGetDateTaken.StartInfo.CreateNoWindow = True
+
+        ExifToolGetDateTaken.Start()
+        Dim result As String = ExifToolGetDateTaken.StandardOutput.ReadToEnd()
+        ExifToolGetDateTaken.Close()
+
+        Return result.Contains("1 image files updated")
+    End Function
+
+    Private Function RotateImageRight(fullFilepath As String) As Boolean
+        Dim orientation As Integer
+        Dim new_orientation As Integer
+        orientation = GetImageOrientation(fullFilepath)
+        Select Case orientation
+            Case 0
+                new_orientation = 6
+            Case 1
+                new_orientation = 6
+            Case 2
+                new_orientation = 7
+            Case 3
+                new_orientation = 8
+            Case 4
+                new_orientation = 5
+            Case 5
+                new_orientation = 2
+            Case 6
+                new_orientation = 3
+            Case 7
+                new_orientation = 4
+            Case 8
+                new_orientation = 1
+        End Select
+
+        Dim success As Boolean = SetImageOrientation(fullFilepath, new_orientation)
+        Return success
+    End Function
+
+    Private Function RotateImageLeft(fullFilepath As String) As Boolean
+        Dim orientation As Integer
+        Dim new_orientation As Integer
+        orientation = GetImageOrientation(fullFilepath)
+        Select Case orientation
+            Case 0
+                new_orientation = 8
+            Case 1
+                new_orientation = 8
+            Case 2
+                new_orientation = 5
+            Case 3
+                new_orientation = 6
+            Case 4
+                new_orientation = 7
+            Case 5
+                new_orientation = 4
+            Case 6
+                new_orientation = 1
+            Case 7
+                new_orientation = 2
+            Case 8
+                new_orientation = 3
+        End Select
+
+
+        Dim success As Boolean = SetImageOrientation(fullFilepath, new_orientation)
+        Return success
+    End Function
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
+
+    Private Sub RotateLeftButton1_Click(sender As Object, e As EventArgs) Handles RotateLeftButton1.Click
+        If File.Exists(loaded_image_filepath) Then
+            RotateImageLeft(loaded_image_filepath)
+            DisplayImages()
+        End If
+    End Sub
+
+    Private Sub RotateRightButton1_Click(sender As Object, e As EventArgs) Handles RotateRightButton1.Click
+        If File.Exists(loaded_image_filepath) Then
+            RotateImageRight(loaded_image_filepath)
+            DisplayImages()
+        End If
+    End Sub
+
+    Private Sub RotateLeftButton2_Click(sender As Object, e As EventArgs) Handles RotateLeftButton2.Click
+        If File.Exists(loaded_secondary_image_filepath) Then
+            RotateImageLeft(loaded_secondary_image_filepath)
+            DisplayImages()
+        End If
+    End Sub
+
+    Private Sub RotateRightButton2_Click(sender As Object, e As EventArgs) Handles RotateRightButton2.Click
+        If File.Exists(loaded_secondary_image_filepath) Then
+            RotateImageRight(loaded_secondary_image_filepath)
+            DisplayImages()
+        End If
+    End Sub
+End Class
+
+
+
